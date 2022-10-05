9ad964a59fd35aa55d9072d914aa14c9d1619834
zehraatessonmez <49163485+zehraatessonmez@users.noreply.github.com>
2022-10-05 14:30:03 +0300

    Initial commit

---
+++ app/src/main/java/com/zehraatessonmez/tasarimcalismasiiki/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zehraatessonmez.tasarimcalismasiiki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
